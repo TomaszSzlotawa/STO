a9d872f57b1ad6d1669921da5cc6189bab110ebd
TomaszSzlotawa <szlotawa.tomasz@gmail.com>
2023-11-21 18:42:23 +0100

    stworzono plik forms.py, zrobiłem urls do widoku signup

---
+++ clubs/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30,required=True, help_text="wpisz swoje imię")
+    last_name = forms.CharField(max_length=30,required=True, help_text="wpisz swoje nazwisko")
+    email = forms.EmailField(max_length=254,help_text="Wprowadź swój adres email")
+
+    class Meta:
+        model = User
+        fields = ('username','email','first_name','last_name','password1','password2')
+
--- clubs/views.py
+++ clubs/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import UsersClub, Club, Team
 
-
+def signup(request):
+    pass
 
 def index(request):
     clubs = Club.objects.all()
     usersClubs = UsersClub.objects.all()
     teams = Team.objects.all()
     return render(request,'clubs\main.html',{'clubs':clubs,'usersClubs':usersClubs,'teams':teams})
+
