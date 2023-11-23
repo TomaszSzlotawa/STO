0247948922bc9a780cb59a97f25c54c1a0c3c039
TomaszSzlotawa <szlotawa.tomasz@gmail.com>
2023-11-23 09:29:47 +0100

    dodałem walidację formularzy pod kątem unikalności adresów mailowych

--- clubs/forms.py
+++ clubs/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+
+
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30,required=True, help_text="wpisz swoje imię")
     last_name = forms.CharField(max_length=30,required=True, help_text="wpisz swoje nazwisko")
@@ -12,10 +14,21 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username','email','first_name','last_name','password1','password2')
 
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("An user with this email already exists!")
+        return email
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username','email','first_name','last_name')
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("An user with this email already exists!")
+        return email
 
 class ProfileForm(forms.ModelForm):
 
